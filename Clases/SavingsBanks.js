762fcbf5ac911007ffe9516b38cf33cb57919175
TomasClur <toclur@pioix.edu.ar>
2025-07-09 22:09:48 -0300

    punto 17 y 18
    andar andaría

--- Clases/SavingsBanks.js
+++ Clases/SavingsBanks.js
@@ -2,7 +2,7 @@ let savingsBanksId = 1;
 //Es un número único de 22 dígitos que identifica la cuenta
 let cbuCounter = 1000000000000000000000;
 
-class SavingsBanks{
+class SavingsBanks {
     /**
      * 
      * @param {string} currency 
@@ -11,14 +11,14 @@ class SavingsBanks{
      */
     //Ponemos el límite como último parámetro para no tener que ingresarlo si está en USD.
     //Pero si le pasamos 0 no hay problema.
-    constructor(currency, alias, limit){
+    constructor(currency, alias, limit) {
         this.id = savingsBanksId;
         this.cbu = cbuCounter;
         cbuCounter++;
         savingsBanksId++;
         this.currency = currency;
         this.balance = 0;
-        if(currency == "ARS") {
+        if (currency == "ARS") {
             this.limit = limit;
             this.overdraft = 0;
         }
@@ -26,11 +26,32 @@ class SavingsBanks{
         this.movements = [];
         this.alias = alias;
     }
+    depositarDinero(dinero_a_agregar) {
+        this.balance += dinero_a_agregar
+        return this.balance
+    }
+    extraerDinero(monto_a_retirar) {
+        if (this.currency !== "ARS") {
+            if (monto_a_retirar > this.balance) {
+                return 0
+            } else {
+                this.balance -= monto_a_retirar
+                return 1
+            }
+        } else {
+            if (monto_a_retirar > this.balance || this.balance + this.overdraft > monto_a_retirar) {
+                return 0
+            }else{
+                this.balance -= monto_a_retirar
+                return 1
+            }
+        }
+    }
 }
-clients[0].savingsBanks.push(new SavingsBanks("ARS","pepito",1000000))
-clients[0].savingsBanks.push(new SavingsBanks("USD","pepito",1000000))
-clients[1].savingsBanks.push(new SavingsBanks("ARS","john",10000))
-clients[1].savingsBanks.push(new SavingsBanks("USD","john",100000))
-clients[2].savingsBanks.push(new SavingsBanks("ARS","primo.de.pepito",10000000))
-clients[2].savingsBanks.push(new SavingsBanks("INR","primo.de.pepito",100000000))
-clients[3].savingsBanks.push(new SavingsBanks("ARS","Jorge",100000))
+clients[0].savingsBanks.push(new SavingsBanks("ARS", "pepito", 1000000))
+clients[0].savingsBanks.push(new SavingsBanks("USD", "pepito",))
+clients[1].savingsBanks.push(new SavingsBanks("ARS", "john", 10000))
+clients[1].savingsBanks.push(new SavingsBanks("USD", "john",))
+clients[2].savingsBanks.push(new SavingsBanks("ARS", "primo.de.pepito", 10000000))
+clients[2].savingsBanks.push(new SavingsBanks("INR", "primo.de.pepito",))
+clients[3].savingsBanks.push(new SavingsBanks("ARS", "Jorge",))
